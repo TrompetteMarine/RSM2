c5094f6a4d1663d5faf76561ba8aa41dd28c50e0
TrompetteMarine <102915139+TrompetteMarine@users.noreply.github.com>
2023-12-31 03:16:01 +0100

    feat(classes): define classes for later analysis

---
+++ classes/score.py
@@ -0,0 +1,7 @@
+class score(object):
+ def __init__(self, positive, negative, positiveWords, negativeWords): 
+    self.positive = positive 
+    self.negative = negative
+    self.positiveWords = positiveWords
+    self.negativeWords = negativeWords
+   
